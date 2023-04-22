ee6b3fc59241343e4903fcc2d33b5c21eb66079c
naitikshrma <iamnaitik10@gmail.com>
2023-04-22 23:08:37 +0530

    Completed the project

--- Expanding_Cards/script.js
+++ Expanding_Cards/script.js
@@ -1,25 +1,30 @@
-//onst panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel')
 
-//panels.forEach(panel => {
-  //  panel.addEventListener('click', () => {
-    //    removeActiveClasses()
-      //  panel.classList.add('active')
-    //})
-//})
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
 
-//function removeActiveClasses() {
-  //  panels.forEach(panel => {
-    //    panel.classList.remove('active')
-    //})
-//}
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
 
-const cards = document.querySelectorAll('.card');
+
+//     ChatGPT version
+
+
+
+/*const cards = document.querySelectorAll('.card');
 
 cards.forEach(card => {
 	card.addEventListener('click', () => {
 		// Close previously expanded card
 		closeExpandedCard();
-
+/ 
 		// Expand current card
 		card.classList.add('expanded');
 	});
@@ -30,4 +35,4 @@ function closeExpandedCard() {
 	if (expandedCard) {
 		expandedCard.classList.remove('expanded');
 	}
-}
+}*/
