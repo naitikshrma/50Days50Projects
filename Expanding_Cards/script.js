96eb071edc2dbada36dc553b7104ec2260d72b8f
naitikshrma <iamnaitik10@gmail.com>
2023-04-22 06:01:43 +0530

    code completed wih alternate code snippets

---
+++ Expanding_Cards/script.js
@@ -0,0 +1,33 @@
+//onst panels = document.querySelectorAll('.panel')
+
+//panels.forEach(panel => {
+  //  panel.addEventListener('click', () => {
+    //    removeActiveClasses()
+      //  panel.classList.add('active')
+    //})
+//})
+
+//function removeActiveClasses() {
+  //  panels.forEach(panel => {
+    //    panel.classList.remove('active')
+    //})
+//}
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+	card.addEventListener('click', () => {
+		// Close previously expanded card
+		closeExpandedCard();
+
+		// Expand current card
+		card.classList.add('expanded');
+	});
+});
+
+function closeExpandedCard() {
+	const expandedCard = document.querySelector('.expanded');
+	if (expandedCard) {
+		expandedCard.classList.remove('expanded');
+	}
+}
